e765188c3e76bc9b7909868b26965d3c2035cfdc
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-16 10:59:46 -0300

    Adicionando remove e ordenação

--- exercicios-listas/src/br/com/sbmtechnology/TesteTarefa.java
+++ exercicios-listas/src/br/com/sbmtechnology/TesteTarefa.java
@@ -1,6 +1,8 @@
 package br.com.sbmtechnology;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TesteTarefa {
@@ -9,15 +11,34 @@ public class TesteTarefa {
 		
 		Tarefa t1 = new Tarefa("Tarefa 1", "Arrumar a casa", 20);
 		Tarefa t2 = new Tarefa("Tarefa 2", "Estrudar", 19);
-		Tarefa t3 = new Tarefa("Tarefa 3", "Tirar o lixo", 25);
+		Tarefa t3 = new Tarefa("Tarefa 3", "Tirar o lixo", 18);
+		Tarefa t4 = new Tarefa("Tarefa 4", "Pagar faculdade", 29);
+		
 		
 		List<Tarefa> tarefas = new ArrayList<>();
 		tarefas.add(t1);
 		tarefas.add(t2);
 		tarefas.add(t3);
 		
-		System.out.println(tarefas);
 		
+		System.out.println(tarefas);
+		// Remover tarefa
+		tarefas.remove(t2);
+        // Adicionar tarefa
+		tarefas.add(t4);
+		// Atualizar tarefa
+        t3.setDescricao("Levar o lixo para fora");
+        
+        
+        System.out.println(tarefas);
+        // Ordenar lista de tarefas pela data limite
+        Collections.sort(tarefas, Comparator.comparingInt(Tarefa::getDataLimite));
+	
+		
+		 // Exibir lista de tarefas
+        for (Tarefa tarefa : tarefas) {
+            System.out.println(tarefa);
+        }
 	}
 
 }
